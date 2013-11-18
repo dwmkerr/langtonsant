ee147ec833b00aa51e4834026f27f306425e7f7d
Dave Kerr <dwmkerr@gmail.com>
2013-11-18 22:04:30 +0100

    Created the simulation code, now we just need a render function and a loop, then the settings...

---
+++ js/langtonsant.js
@@ -0,0 +1,120 @@
+/*
+	Langton's Ant
+
+	The Langton's Ant class represents a Langton's Ant simulation. It is 
+	initialised with a call to 'Initialise', providing all configuration
+	and erasing any state. The simulation can then be 'ticked', forwards
+	or backwards.
+*/
+
+function LangtonsAnt() {
+
+	//	First, create all of the variables we use in the class.
+
+	//	The position of the ant.
+	this.antPosition = {
+		x: 0,
+		y: 0
+	};
+
+	//	The direction of the ant, in degrees clockwise from north.
+	this.antDirection = 0;
+
+	//	A set of all tiles. The value for each tile is its state.
+	this.tiles = [];
+
+	//	The bounds of the system.
+	this.bounds = {
+		xMin: 0,
+		xMax: 0,
+		yMin: 0,
+		yMax: 0
+	};
+
+	//	Gets a tile state.
+	this.getTile = function(x, y) {
+		if(this.states[x] === undefined) {
+			this.states[x] = [];
+		}
+		var state = this.states[x][y];
+		return state === undefined ? 0 : state;
+	};
+
+	//	Set a tile state.
+	this.setTile = function(x, y, state) {
+		if(this.tiles[x] === undefined) {
+			this.tiles[x] = [];
+		}
+		this.tiles[x][y] = state;
+
+		//	Update the bounds of the system.
+		if(x < this.bounds.xMin) {this.bounds.xMin = 0;}
+		if(x > this.bounds.xMax) {this.bounds.xMin = 0;}
+		if(y < this.bounds.yMin) {this.bounds.yMax = 0;}
+		if(y > this.bounds.yMax) {this.bounds.yMax = 0;}
+	};
+
+	//	Advance a tile states.
+	this.advanceTile = function(x, y) {
+		var state = this.getTile(x, y);
+		state++;
+		if(state == states.length()) {
+			state = 0;
+		}
+		this.setTile(x, y, state);
+	};
+
+	//	Take a step forwards.
+	this.stepForwards = function() {
+
+		//	Get the state of the tile that the ant is on and update that tile.
+		var state = this.getState(this.antPosition.x, this.antPosition.y);
+		this.advanceTile(this.antPosition.x, this.antPosition.y);
+
+		//	Change direction.
+		if(state === 'L') {
+			this.antDirection -= 90;
+		} else if(state === 'R') {
+			this.antDirection += 90;
+		}
+		this.antDirection %= 360;
+
+		//	Move the ant.
+		if(this.antDirection === 0) {
+			this.anyPosition.y++;
+		} else if (this.antDirection === 90 || this.antDirection === -270) {
+			this.antPosition.x++;
+		} else if (this.antDirection === 180 || this.antDirection === -180) {
+			this.antPosition.y--;
+		}
+		else {
+			this.antPosition.x--;
+		}
+
+	};
+
+	this.initialise = function(configuration) {
+
+		//	Reset the tiles, ant and states.
+		this.antPosition = {
+			x: 0,
+			y: 0
+		};
+		this.antDirection = 0;
+		this.tiles = [];
+		this.bounds = {
+			xMin: 0,
+			xMax: 0,
+			yMin: 0,
+			yMax: 0
+		};
+		this.states = [];
+
+		//	If we have no states, create our own.
+		if(configuration.states !== undefined) {
+			this.states = configuration.states;
+		} else {
+			this.states = ['L', 'R'];
+		}
+	};
+}
